27477edf85f4379a48b529091c61e9f33756653e
ManU4kym <emmannjoroge36@gmail.com>
2025-01-14 23:48:18 +0300

    components/Layouts  = app logo and search bar

---
+++ components/TopNav.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="TopNav" class="fixed bg-white z-30 flex items-center w-full border-b h-[61px]">
+        <div :class="route.fullPath === '/' ? 'max-w-[1150px]' : ''"
+            class="flex items-center justify-between w-full px-6 mx-auto">
+            <div :class="route.fullPath === '/' ? 'w-[80%]' : 'lg:w-[20%] w-[70%]'">
+                <nuxt-link to="/" class="flex justify-start ">
+                    <img src="https://cdn-icons-png.freepik.com/256/2549/2549120.png?semt=ais_hybrid" alt="Logo"
+                          class="h-[38px] w-[38px] " />
+                </nuxt-link>
+            </div>
+            <div class="hidden md:flex items-center bg-[#F1F1F2] p-1 rounded-full max-w-[380px] w-full">
+                <input type="text" class="w-full pl-3 my-2 bg-transparent placeholder-[#838383] text-[15px] focus:outline-none" placeholder="Search accounts">
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+const route = useRoute()
+</script>
+
+<style scoped>
+/* Styles here */
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="bg-black-500 m-8 leading-10 p-2 ">
-        testing mzee
+    <MainLayout></MainLayout>
 
-        <Icon name="uil:github" style="color: green" size="100" />
-        <Icon name="uil:golf-ball" class="bg-green-400" size="100" />
 
-    </div>
 </template>
+
+<script setup>
+import MainLayout from '~/layouts/MainLayout.vue'
+</script>
