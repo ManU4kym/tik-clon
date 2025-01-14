53956eb7d026b4680af68f4b2c48d8e8e844c754
ManU4kym <emmannjoroge36@gmail.com>
2025-01-14 23:14:27 +0300

    packages

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="bg-black-500 m-8 leading-10 p-2 ">
         testing mzee
-        <Icon name="X mdi:close" size="100" />
+
+        <Icon name="uil:github" style="color: green" size="100" />
+        <Icon name="uil:golf-ball" class="bg-green-400" size="100" />
+
     </div>
 </template>
