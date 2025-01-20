34a37cef686c83c04701bada281e1e1b399fb2ed
ManU4kym <emmannjoroge36@gmail.com>
2025-01-20 22:02:25 +0300

    sidenav partially complete

--- components/MenuItemFollow.vue
+++ components/MenuItemFollow.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="flex items-center hover:bg-gray-100 rounded-md w-full py-1.5 px-2">
-        <img src="https://cdn-icons-png.flaticon.com/128/18572/18572598.png" alt="" class="rounded-full lg:mx-0 mx-auto" width="35">
+        <img src="https://cdn-icons-png.flaticon.com/128/18572/18572598.png" alt="" class="rounded-full lg:mx-0 mx-auto"
+            width="35">
 
         <div class="lg:pl-2 5 lg:block hidden">
             <div class="flex items-center">
@@ -9,12 +10,15 @@
                 </div>
                 <div class="ml-1 rounded-full relative bg-[#58D5EC] h-[14px]">
                     <Icon class="relative -top-[4px]" name="teenyicons:tick-small-solid" color="#FFFFFF" size="15" />
-                 </div>
+                </div>
+            </div>
+            <div class="font-light text-[12px] text-gray-600">
+                User name
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
-   
+defineProps(['user'])
 </script>
--- components/SideNavMain.vue
+++ components/SideNavMain.vue
@@ -1,11 +1,12 @@
 <template>
-    <div :class="route.fullPath === '/' ? 'lg:w-[310px]' : 'lg:w-[220px]'" id="SideNavMain" class="fixed z-20 bg-white pt-[70px] h-full lg:border-r-0 border-r w-[75px] overflow-auto">
+    <div :class="route.fullPath === '/' ? 'lg:w-[310px]' : 'lg:w-[220px]'" id="SideNavMain"
+        class="fixed z-20 bg-white pt-[70px] h-full lg:border-r-0 border-r w-[75px] overflow-auto">
         <div class="lg-full mx-auto w-[55px]">
             <NuxtLink to="/">
-                <MenuItem iconString= "For You" class="whitespace-nowrap" colorString="text-[#C5283D]" sizeString="30" />
+                <MenuItem iconString="For You" class="whitespace-nowrap" colorString="text-[#C5283D]" sizeString="30" />
             </NuxtLink>
-            <MenuItem iconString= "Following" colorString="text-[#000000]"  sizeString="27"/>
-            <MenuItem iconString= "LIVE" colorString="text-[#000000]" sizeString="27"/>
+            <MenuItem iconString="Following" colorString="text-[#000000]" sizeString="27" />
+            <MenuItem iconString="LIVE" colorString="text-[#000000]" sizeString="27" />
 
 
             <div class="border-b lg:ml-8 mt-2" />
@@ -13,11 +14,37 @@
             <div class="lg:block hidden text-xs text-gray-600 font-semibold pt-4 pb-2 px-2">
                 Suggested accounts
             </div>
-            <div class="lg:hidden block pt-3"/>
+            <div class="lg:hidden block pt-3" />
 
             <div class="cursor-pointer">
-                <MenuItemFollow/>
+                <MenuItemFollow />
             </div>
+
+            <button class="lg:block hidden pt-1.5 pl-2 text-[13px] text-[#F02C56]">
+                See all
+            </button>
+
+            <div class="border-b lg:ml-2 mt-2" />
+
+            <div class="lg:block hidden text-xs text-gray-600 font-semibold pt-4 pb-2 px-2">
+                Following accounts
+            </div>
+            <div class="cursor-pointer">
+                <MenuItemFollow />
+            </div>
+
+            <button class="lg:block hidden pt-1.5 pl-2 text-[13px] text-[#F02C56]">
+                See more
+            </button>
+            <div class="lg:block hidden border-b lg:ml-2 mt-2" />
+
+            <div class="lg:block hidden  text-gray-500 text-[11px]">
+                <div class="pt-4 px-2 ">About Newsroom TikTac Shop Contact Careers ByteDance</div>
+                <div class="pt-4 px-2">TikTac for Good Advertise Developers Transparency TikTac Rewards</div>
+                <div class="pt-4 px-2">Help Safety Terms Privacy Creator Portal Community Guidelines</div>
+                <div class="pt-4 px-2">&copy; 2023 TikTac</div>
+            </div>
+            <div class="pb-14"></div>
         </div>
     </div>
 </template>
