41da5ad5b2811683870333abfaa79b786b77fb3b
ManU4kym <emmannjoroge36@gmail.com>
2025-01-20 22:21:58 +0300

    posts

---
+++ components/PostMain.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="PostMain" class="flex border-b py-6">
+        <div class="cursor-pointer">
+            <img src="https://cdn-icons-png.flaticon.com/128/18717/18717265.png" alt=""
+                class="rounded-full max-h-[60px]" width="60">
+        </div>
+        <div class="pl-3 w-full px-4">
+            <div class="flex items-center justify-between pb-0 5">
+                <button>
+                    <span class="font-bold hover-underline cursor-pointer">
+                        user name
+                    </span>
+                    <span class="text-light pl-1 cursor-painter text-gray-500 text-[13px]">
+                        @user name
+                    </span>
+                </button>
+                <button
+                    class="border py-0 5 rounded-md font-semibold hover:bg-[#ffeef2] border-[#F02C56] text-[#F02C56] text-[15px] px-[21px]">Follow</button>
+            </div>
+            <div class="text-[15px] pb-0.5 break-words md:max-w-[400px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus
+                ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
+                mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+            </div>
+            <div class="text-[14px] text-gray-500 pb-0.5">
+                #fun #cool #SuperAwesome
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-    <MainLayout></MainLayout>
+    <MainLayout>
+        <div class="pt-[80px] w-[calc(100%-90px)] max-w-[690px]">
+            <PostMain />
+        </div>
+    </MainLayout>
 
 
 </template>
 
 <script setup>
 import MainLayout from '~/layouts/MainLayout.vue'
+import PostMain from '~/components/PostMain.vue'
 </script>
