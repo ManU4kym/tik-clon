a92a5afdbaaed63261906bcd7b86c8c2be2ebd4f
ManU4kym <emmannjoroge36@gmail.com>
2025-01-21 23:53:45 +0300

    main page meta

--- pages/index.vue
+++ pages/index.vue
@@ -11,4 +11,20 @@
 <script setup>
 import MainLayout from '~/layouts/MainLayout.vue'
 import PostMain from '~/components/PostMain.vue'
+
+useHead({
+    title: 'Tik Tac',
+    meta: [
+        {
+            name: 'description',
+            content: 'Awesome app'
+        }
+    ],
+    link: [
+        {
+            rel: 'icon',
+            href: 'https://cdn-icons-png.flaticon.com/128/4424/4424479.png', // Update with the correct path to your favicon
+        },
+    ],
+})
 </script>
