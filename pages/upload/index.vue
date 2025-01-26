ae911fee35aeb5cafc4067797a2079d148128cd5
@Versatilekim <emmannjoroge36@gmail.com>
2025-01-26 10:32:42 +0000

    casing

--- pages/upload/index.vue
+++ pages/upload/index.vue
@@ -8,7 +8,7 @@
             </div>
 
             <div class="mt-8 md:flex gap-6">
-                <label for="fileinput"
+                <label v-if="false" for="fileinput"
                     class="md:mx-0 mx-auto mt-4 mb-6 flex flex-col items-center justify-center w-full max-w-[260px] h-[470px] text-center p-3 border-2 border-dashed border-gray-300 rounded-lg hover:bg-gray-100 cursor-pointer">
                     <Icon name="majesticons:cloud-upload" size="40" class="text-[#b3b3b1]" />
                     <div class="mt-4 text-[17px]">
@@ -23,6 +23,25 @@
                     </div>
                     <input type="file" ref="file" id="fileinput" hidden accept=".mp4">
                 </label>
+                <div v-else
+                    class="md:mx-0 mx-auto mt-4 md:mb-12 mb-16 flex items-center justify-center w-full max-w-[260px] h-[540px] p-3 rounded-2xl cursor-pointer relative">
+
+                    <div class="bg-black w-full h-full" />
+
+                    <img src="~/assets/images/mobile-case.png" alt="" class="absolute z-20  pointer-events-none h-full">
+                    <img src="~/assets/images/tiktok-logo-white.png" alt="" class="absolute right-4 bottom-6 z-20"
+                        width="90">
+                    <video autoplay loop muted src="https://cdn.pixabay.com/video/2024/12/26/248896_tiny.mp4"
+                        class="absolute rounded-xl object-cover z-10 w-full h-full p-[13px]" />
+                    <div
+                        class="absolute -bottom-12 flex items-center justify-between z-50 rounded-xl border w-full p-2 border-gray-300">
+                        <div class="flex items-center truncate">
+                            <Icon name="clarity:success-standard-line" size="16" class="min-w-[16px]" />
+                            <div class="pl-1 truncate text-ellipsis text-[11px]">Video name</div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </UploadLayout>
