3076536f057fcd9d0db146bcf0bf479c016640e9
@Versatilekim <emmannjoroge36@gmail.com>
2025-01-27 16:04:19 +0000

    side

--- components/SideNavMain.vue
+++ components/SideNavMain.vue
@@ -1,7 +1,7 @@
 <template>
     <div :class="route.fullPath === '/' ? 'lg:w-[310px]' : 'lg:w-[220px]'" id="SideNavMain"
         class="fixed z-20 bg-white pt-[70px] h-full lg:border-r-0 border-r w-[75px] overflow-auto">
-        <div class="lg-full mx-auto w-[55px]">
+        <div class="lg:w-full lg:border-r mx-auto w-[55px]">
             <NuxtLink to="/">
                 <MenuItem iconString="For You" class="whitespace-nowrap" colorString="text-[#C5283D]" sizeString="30" />
             </NuxtLink>
@@ -9,7 +9,7 @@
             <MenuItem iconString="LIVE" colorString="text-[#000000]" sizeString="27" />
 
 
-            <div class="border-b lg:ml-8 mt-2" />
+            <div class="border-b lg:ml-2 lg:mr-4 mt-2" />
 
             <div class="lg:block hidden text-xs text-gray-600 font-semibold pt-4 pb-2 px-2">
                 Suggested accounts
@@ -24,7 +24,7 @@
                 See all
             </button>
 
-            <div class="border-b lg:ml-2 mt-2" />
+            <div class="border-b lg:ml-2 mt-2 lg:mr-4" />
 
             <div class="lg:block hidden text-xs text-gray-600 font-semibold pt-4 pb-2 px-2">
                 Following accounts
@@ -36,9 +36,9 @@
             <button class="lg:block hidden pt-1.5 pl-2 text-[13px] text-[#F02C56]">
                 See more
             </button>
-            <div class="lg:block hidden border-b lg:ml-2 mt-2" />
+            <div class="lg:block hidden border-b lg:ml-2 mt-2 lg:mr-4" />
 
-            <div class="lg:block hidden  text-gray-500 text-[11px]">
+            <div class="lg:block hidden   text-gray-500 text-[11px]">
                 <div class="pt-4 px-2 ">About Newsroom TikTac Shop Contact Careers ByteDance</div>
                 <div class="pt-4 px-2">TikTac for Good Advertise Developers Transparency TikTac Rewards</div>
                 <div class="pt-4 px-2">Help Safety Terms Privacy Creator Portal Community Guidelines</div>
--- components/TopNav.vue
+++ components/TopNav.vue
@@ -5,11 +5,13 @@
             <div :class="route.fullPath === '/' ? 'w-[80%]' : 'lg:w-[20%] w-[70%]'">
                 <nuxt-link to="/" class="flex justify-start ">
                     <img src="https://cdn-icons-png.freepik.com/256/2549/2549120.png?semt=ais_hybrid" alt="Logo"
-                          class="h-[38px] w-[38px] " />
+                        class="h-[38px] w-[38px] " />
                 </nuxt-link>
             </div>
             <div class="hidden md:flex items-center bg-[#F1F1F2] p-1 rounded-full max-w-[380px] w-full">
-                <input type="text" class="w-full pl-3 my-2 bg-transparent placeholder-[#838383] text-[15px] focus:outline-none" placeholder="Search accounts">
+                <input type="text"
+                    class="w-full pl-3 my-2 bg-transparent placeholder-[#838383] text-[15px] focus:outline-none"
+                    placeholder="Search accounts">
                 <div class="px-3 py-1 flex items-center border-l border-l-gray-300">
                     <Icon name="uil:search" style="color: black" />
                 </div>
@@ -23,24 +25,29 @@
                     <button class="flex items-center bg-[#F02C56] text-white border rounded-md px-3 py-[6px]">
                         <span class="mx-4 font-medium text-[15px]">Log in</span>
                     </button>
-                       <Icon name="mdi:dots-vertical" color="#161724" size="25"/>
+                    <Icon name="mdi:dots-vertical" color="#161724" size="25" />
                 </div>
                 <div class="flex items-center">
-                    <Icon class="ml-1 mr-4" name="carbon:send-alt" color="#161724" size="25"/>
-                    <Icon class="mr-5" name="bx:message-detail" color="#161724" size="25"/>
+                    <Icon class="ml-1 mr-4" name="carbon:send-alt" color="#161724" size="25" />
+                    <Icon class="mr-5" name="bx:message-detail" color="#161724" size="25" />
                     <div class="relative">
                         <button class="mt-1">
-                            <img @click="showMenu = !showMenu" src="https://img.freepik.com/free-photo/3d-render-vs-company-metal-letter-logo-pen-tool-created-clipping-path-included-jpeg-easy-composite_460848-7192.jpg?ga=GA1.1.2111888000.1736886517&semt=ais_hybrid" alt="profile" class="rounded-full h-[38px] w-[48px] "  >
+                            <img @click="showMenu = !showMenu"
+                                src="https://img.freepik.com/free-photo/3d-render-vs-company-metal-letter-logo-pen-tool-created-clipping-path-included-jpeg-easy-composite_460848-7192.jpg?ga=GA1.1.2111888000.1736886517&semt=ais_hybrid"
+                                alt="profile" class="rounded-full h-[38px] w-[48px] ">
                         </button>
 
-                        <div v-if="showMenu" id="popupmenu" class="absolute bg-white rounded-lg py-1.5 w-[200px] shadow-xl border top-[43px] -right-2">
-                            <nuxt-link @click="$event => showMenu = false" to="" class="flex items-center justify-start py-3 px-2 hover:bg-gray-100 cursor-pointer">
-                                <Icon name="uil:user" size="20"/>
+                        <div v-if="showMenu" id="popupmenu"
+                            class="absolute bg-white rounded-lg py-1.5 w-[200px] shadow-xl border top-[43px] -right-2">
+                            <nuxt-link @click="$event => showMenu = false" to=""
+                                class="flex items-center justify-start py-3 px-2 hover:bg-gray-100 cursor-pointer">
+                                <Icon name="uil:user" size="20" />
                                 <span class="pl-2 font-semi bold text-sm">Profile</span>
                             </nuxt-link>
 
-                            <div  class="flex items-center justify-start py-3 px-1.5 hover:bg-gray-100 border-t cursor-pointer">
-                                <Icon name="ic:outline-login" size="20"/>
+                            <div
+                                class="flex items-center justify-start py-3 px-1.5 hover:bg-gray-100 border-t cursor-pointer">
+                                <Icon name="ic:outline-login" size="20" />
                                 <span class="pl-2 font-semi bold text-sm">Logout</span>
                             </div>
                         </div>
